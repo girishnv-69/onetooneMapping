d3c3f6086a3f67e598920b0375bde2022532a112
girishnv-69 <girishgiri246@gmail.com>
2023-06-03 12:35:42 +0530

    first commit

---
+++ src/main/java/com/by/person/Persone.java
@@ -0,0 +1,56 @@
+
+package com.by.person;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Persone {
+
+	@Id
+	private int id;
+	private String name;
+	private  int age;
+	private String gender;
+	private long phno;
+//	@OneToOne(fetch = FetchType.LAZY)
+//	private AdharCard card;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+//	public AdharCard getCard() {
+//		return card;
+//	}
+//	public void setCard(AdharCard card) {
+//		this.card = card;
+//	}
+	
+	
+}
